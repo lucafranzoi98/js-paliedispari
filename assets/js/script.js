cf938f413f9b2a2845010a575dfb8e17c02a0b16
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-08-31 15:13:14 +0200

    add sum function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,45 @@
 
 const buttonEvenEl = document.getElementById("button-even");
 
+
+/**
+ * Generate a random number between min and max
+ * @param {number} min 
+ * @param {number} max 
+ * @returns {number}
+ */
+
 function getRndInteger(min, max) {
    const number = Math.floor(Math.random() * (max - min + 1) ) + min;
    return number;
 }
 
-buttonEvenEl.addEventListener("click", function(e){
-   const inputUserEl = document.getElementById("input-user");
-   console.log(inputUserEl.value);
+/**
+ * Sum two numbers
+ * @param {number} numComputer 
+ * @param {number} numUser 
+ * @returns {number}
+ */
 
-   const pcNumber = getRndInteger(1, 5);
-   console.log(pcNumber);
+function sum(numComputer, numUser) {
+   const sum = numComputer + numUser;
+   return sum;
+}
+
+buttonEvenEl.addEventListener("click", function(e){
+   const inputUserEl = Number(document.getElementById("input-user").value);
+
+   if (inputUserEl < 1 || inputUserEl > 5) {
+      alert("Inserisci un numero da 1 a 5!")
+   } else {
+      console.log(inputUserEl);
+
+      const pcNumber = getRndInteger(1, 5);
+      console.log(pcNumber);
+
+      const totalSum = sum(inputUserEl, pcNumber);
+      console.log(totalSum);
+   }
 
    e.preventDefault();
 })
